612dfa692afb23091e2afa5a358f0a72302b73ae
amineboukraim <amineboukraim247@gmail.com>
2026-08-16 12:07:05 +0100

    github_pages_workflow

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  basePath: isGithubActions ? '/trabaf-construction-tunisie' : '',
 };
 
 export default nextConfig;
